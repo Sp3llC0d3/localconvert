57c17c50357aec1af1563249ee56a0c0b3ef1121
Sp3llC0d3 <Sp3llC0d3@users.noreply.github.com>
2026-03-22 16:38:49 +0300

    Clean up About page - remove VERT team, sponsors, donations, update credits

--- src/lib/sections/about/Resources.svelte
+++ src/lib/sections/about/Resources.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import Panel from "$lib/components/visual/Panel.svelte";
-	import { CONTACT_EMAIL, DISCORD_URL, GITHUB_URL_VERT } from "$lib/util/consts";
+	import { CONTACT_EMAIL, GITHUB_URL } from "$lib/util/consts";
 	import { effects } from "$lib/store/index.svelte";
 	import {
 		GithubIcon,
 		LinkIcon,
 		MailIcon,
-		MessageCircleMoreIcon,
 	} from "lucide-svelte";
 	import { m } from "$lib/paraglide/messages";
 </script>
@@ -22,18 +21,7 @@
 	</h2>
 	<div class="flex gap-3">
 		<a
-			href={DISCORD_URL}
-			target="_blank"
-			rel="noopener noreferrer"
-			class="btn {$effects
-				? ''
-				: '!scale-100'} flex-1 gap-2 p-4 rounded-full bg-button text-black dynadark:text-white flex items-center justify-center"
-		>
-			<MessageCircleMoreIcon size="24" class="inline-block mr-2" />
-			{m["about.resources.discord"]()}
-		</a>
-		<a
-			href={GITHUB_URL_VERT}
+			href={GITHUB_URL}
 			target="_blank"
 			rel="noopener noreferrer"
 			class="btn {$effects
